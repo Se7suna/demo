7532407be57b228c802149d0d2724ed7e290d496
773561801@qq.com <773561801@qq.com>
2019-08-25 19:25:49 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
+    <transition name="slide-fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -11,7 +13,26 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  .slide-fade {
+    position: absolute;
+    left: 0;
+    right: 0;
+  }
+  .slide-fade-enter-active {
+    transition: all 1.2s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all 0.1s cubic-bezier(2, 0.5, 0.8, 1);
+  }
+  .slide-fade-enter,
+  .slide-fade-leave-to {
+    left: 0;
+    right: 0;
+    transform: translateX(50px);
+    opacity: 0;
+  }
 }
+
 .page {
   padding: 200px 0;
   .list {
